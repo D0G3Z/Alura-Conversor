04d3508384f7f15965036c9c6fabed720c2b5162
DiegoSR <diego.sr24@hotmail.com>
2023-02-21 16:31:43 -0500

    Creando Menú Principal

--- ConversorAlura/src/main/java/Clases/Divisa.java
+++ ConversorAlura/src/main/java/Clases/Divisa.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author rocke
- */
+import javax.swing.JOptionPane;
+
 public class Divisa {
-    
+    public static void main(String[] args) {
+        String [] opciones = {"Conversor de Divisa", "Conversor de Temperatura", "Conversor de Kilometraje", "Conversor de Peso"};
+        JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menú", JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+    }
 }
