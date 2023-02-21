49dc6ec68ea870eedd39750def03d28756d45e3d
DiegoSR <diego.sr24@hotmail.com>
2023-02-21 17:07:27 -0500

    Creando el menú de divisas

--- ConversorAlura/src/main/java/Clases/Divisa.java
+++ ConversorAlura/src/main/java/Clases/Divisa.java
@@ -5,7 +5,16 @@ import javax.swing.JOptionPane;
 
 public class Divisa {
     public static void main(String[] args) {
-        String [] opciones = {"Conversor de Divisa", "Conversor de Temperatura", "Conversor de Kilometraje", "Conversor de Peso"};
-        JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menú", JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+        String [] opPrincipal = {"Conversor de Divisa", "Conversor de Temperatura", "Conversor de Kilometraje", "Conversor de Peso"};
+        String [] opDivisa = {"De Soles a Dólares", "De Soles a Euros", "De Soles a Libras Esterlinas", "De Soles a Yen", "De Soles a Won Coreano",
+        "De Dólares a Soles", "De Euros a Soles", "De Libras Esterlinas a Soles", "De Yen a Soles", "De Won Coreano a Soles"};
+        
+        Object menu = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menú", JOptionPane.PLAIN_MESSAGE, null, opPrincipal, opPrincipal[0]);
+        Object menuDivisa = JOptionPane.showInputDialog(null, "Elija una opción a convertir", "Divisa", JOptionPane.PLAIN_MESSAGE, null, opDivisa, opDivisa[0]);
+        
+        if(menu.toString().equalsIgnoreCase("Conversor de Divisa")){
+            String input = JOptionPane.showInputDialog(menuDivisa);
+            double divisa = Integer.parseInt(input);
+        }
     }
 }
