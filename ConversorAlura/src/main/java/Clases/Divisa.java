582439fbef1e06d1c17be1dfb1b6dff6af6bbfa4
DiegoSR <diego.sr24@hotmail.com>
2023-02-21 19:17:57 -0500

    Creando nuevas cadenas de menús y la opcion de volver una vez acabado

--- ConversorAlura/src/main/java/Clases/Divisa.java
+++ ConversorAlura/src/main/java/Clases/Divisa.java
@@ -5,16 +5,33 @@ import javax.swing.JOptionPane;
 
 public class Divisa {
     public static void main(String[] args) {
-        String [] opPrincipal = {"Conversor de Divisa", "Conversor de Temperatura", "Conversor de Kilometraje", "Conversor de Peso"};
+        String [] opPrincipal = {"Conversor de Divisa", "Conversor de Temperatura", "Conversor de Metros", "Conversor de Peso"};
         String [] opDivisa = {"De Soles a Dólares", "De Soles a Euros", "De Soles a Libras Esterlinas", "De Soles a Yen", "De Soles a Won Coreano",
         "De Dólares a Soles", "De Euros a Soles", "De Libras Esterlinas a Soles", "De Yen a Soles", "De Won Coreano a Soles"};
+        String [] opTemperatura = {};
+        String [] opMetros = {};
+        String [] opPesos = {};
         
-        Object menu = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menú", JOptionPane.PLAIN_MESSAGE, null, opPrincipal, opPrincipal[0]);
-        Object menuDivisa = JOptionPane.showInputDialog(null, "Elija una opción a convertir", "Divisa", JOptionPane.PLAIN_MESSAGE, null, opDivisa, opDivisa[0]);
+        Boolean band = true;
+        while(band == true){
+        Object menu = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menú",
+                JOptionPane.PLAIN_MESSAGE, null, opPrincipal, opPrincipal[0]);
         
-        if(menu.toString().equalsIgnoreCase("Conversor de Divisa")){
-            String input = JOptionPane.showInputDialog(menuDivisa);
+        if(menu.toString() == opPrincipal[0]){       
+            String input = JOptionPane.showInputDialog(JOptionPane.showInputDialog(null, "Elija una opción a convertir", "Divisa",
+                JOptionPane.PLAIN_MESSAGE, null, opDivisa, opDivisa[0]));         
             double divisa = Integer.parseInt(input);
+            JOptionPane.showMessageDialog(null, "" + divisa);
         }
+        
+        int regresar = JOptionPane.showOptionDialog(null, "Desea continuar", "Opción", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, new Object[]{"SI", "NO"}, 0);
+        
+        if(JOptionPane.NO_OPTION == regresar){
+            band = false;
+        }
+        }
+ 
     }
+    
 }
